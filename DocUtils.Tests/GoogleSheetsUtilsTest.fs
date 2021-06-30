204f3a503d976889ded85845e661269053e343ab
yurii-litvinov <yurii.litvinov@gmail.com>
2021-06-30 15:29:57 +0300

    First commit for DocUtils project.

--- DocUtils.Tests/GoogleSheetsUtilsTest.fs
+++ DocUtils.Tests/GoogleSheetsUtilsTest.fs
@@ -1,17 +1,25 @@
 ﻿module DocUtils.Tests
 
 open NUnit.Framework
+open System.IO
+
+let credentials = "../../../../../credentials.json"
 
 [<Test>]
 let SheetServiceShallListCorrectSheetsTest () =
-    use service = new GoogleSheetService("../../../../../credentials.json", "DocUtilsTests")
-    let sheets = service.Spreadsheet("1b1fhGFInVDNXAb_Ok14Nl03V-DviKe-GrE2Geuwsw9o").Sheets()
-    Assert.AreEqual([ "19.Б07"; "19.Б08"; "19.Б09"; "19.Б10" ], sheets)
-
+    if File.Exists credentials then
+        use service = new GoogleSheetService(credentials, "DocUtilsTests")
+        let sheets = service.Spreadsheet("1b1fhGFInVDNXAb_Ok14Nl03V-DviKe-GrE2Geuwsw9o").Sheets()
+        Assert.AreEqual([ "19.Б07"; "19.Б08"; "19.Б09"; "19.Б10" ], sheets)
+    else
+        Assert.Ignore("No redentials for Google Sheets found.")
 
 [<Test>]
-let ReadingTest () =
-    use service = new GoogleSheetService("../../../../../credentials.json", "AssignmentMatcher")
-    let sheet = service.Sheet("1b1fhGFInVDNXAb_Ok14Nl03V-DviKe-GrE2Geuwsw9o", "19.Б07")
-    let column = sheet.ReadColumn "A" 0
-    Assert.AreEqual("ФИО", Seq.head column)
+let ReadingSheetShallGetExpectedValuesTest () =
+    if File.Exists credentials then
+        use service = new GoogleSheetService(credentials, "DocUtilsTests")
+        let sheet = service.Sheet("1b1fhGFInVDNXAb_Ok14Nl03V-DviKe-GrE2Geuwsw9o", "19.Б07")
+        let column = sheet.ReadColumn("A")
+        Assert.AreEqual("ФИО", Seq.head column)
+    else
+        Assert.Ignore("No redentials for Google Sheets found.")
--- DocUtils/GoogleSheetsUtils.fs
+++ DocUtils/GoogleSheetsUtils.fs
@@ -10,17 +10,20 @@ open Google.Apis.Util.Store
 open Google.Apis.Sheets.v4.Data
 
 type Sheet(service: SheetsService, spreadsheetId: string, sheetId: string) =
-    member _.WriteColumn column offset (data: #seq<string>) =
-        let range = sheetId + "!" + column + (string offset) + ":" + column + (string ((data |> Seq.length) + offset))
-        let valueRange = ValueRange(Values = [| data |> Seq.cast<obj> |> Seq.toArray |])
-        valueRange.MajorDimension <- "COLUMNS"
-        let request = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, range)
-        request.ValueInputOption <- Nullable(SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW)
+    let checkOffset offset =
+        if offset <= 0 then 
+            raise <| new ArgumentOutOfRangeException ("Offset shall be greater than zero.")
 
-        request.Execute() |> ignore
+    let checkBoundaries offset size =
+        checkOffset offset
+        if size < offset then 
+            raise <| new ArgumentOutOfRangeException ("size shall be greater than offset.")
 
-    member _.ReadSheet startColumn endColumn offset =
-        let range = sheetId + "!" + startColumn + (string offset) + ":" + endColumn + "1000"
+    let read startColumn endColumn offset size =
+        let offset = defaultArg offset 1
+        let size = defaultArg size 1000
+        checkBoundaries offset size
+        let range = $"{sheetId}!{startColumn}{offset}:{endColumn}{offset + size}"
 
         let request = service.Spreadsheets.Values.Get(spreadsheetId, range)
 
@@ -30,18 +33,31 @@ type Sheet(service: SheetsService, spreadsheetId: string, sheetId: string) =
         else
             Seq.empty
 
-    member this.ReadColumn column offset =
-        let column = this.ReadSheet column column offset
-        column |> Seq.concat
+    member _.WriteColumn(column: string, data: #seq<string>, ?offset: int) =
+        let offset = defaultArg offset 1
+        checkOffset offset
+        let range = sheetId + "!" + column + (string offset) + ":" + column + (string ((data |> Seq.length) + offset))
+        let valueRange = ValueRange(Values = [| data |> Seq.cast<obj> |> Seq.toArray |])
+        valueRange.MajorDimension <- "COLUMNS"
+        let request = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, range)
+        request.ValueInputOption <- Nullable(SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW)
 
-and Spreadsheet(service: SheetsService, spreadsheetId: string) =
+        request.Execute() |> ignore
+
+    member _.ReadSheet(startColumn: string, endColumn: string, ?offset: int, ?size: int) =
+        read startColumn endColumn offset size
+
+    member _.ReadColumn(column: string, ?offset: int, ?size: int) =
+        read column column offset size |> Seq.concat
+
+type Spreadsheet(service: SheetsService, spreadsheetId: string) =
     member _.Sheet (sheetId: string) = Sheet(service, spreadsheetId, sheetId)
 
     member _.Sheets () =
         let spreadsheet = service.Spreadsheets.Get(spreadsheetId).Execute ()
         spreadsheet.Sheets |> Seq.map (fun s -> s.Properties.Title)
 
-and GoogleSheetService(credentialsFileName: string, applicationName: string) =
+type GoogleSheetService(credentialsFileName: string, applicationName: string) =
     let service = 
         use credentialsStream = new FileStream(credentialsFileName, FileMode.Open, FileAccess.Read)
 
