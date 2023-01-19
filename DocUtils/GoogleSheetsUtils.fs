b4cec8d5f52c242e91b7f9381f77695d2ba55ba1
yurii-litvinov <yurii.litvinov@gmail.com>
2023-01-19 22:08:35 +0300

    feat: add writing of range and merge/unmerge cells functions

--- DocUtils/GoogleSheetsUtils.fs
+++ DocUtils/GoogleSheetsUtils.fs
@@ -13,8 +13,8 @@ open System.Threading.Tasks
 /// Represents a single sheet (or tab) in a spreadsheet, provides methods to read and modify data.
 type Sheet internal (service: SheetsService, spreadsheetId: string, sheetId: string) =
     let checkOffset offset =
-        if offset <= 0 then 
-            raise <| ArgumentOutOfRangeException ("Offset shall be greater than zero.")
+        if offset <= 0 then
+            raise <| ArgumentOutOfRangeException("Offset shall be greater than zero.")
 
     let readAsync startColumn endColumn offset size =
         task {
@@ -26,6 +26,7 @@ type Sheet internal (service: SheetsService, spreadsheetId: string, sheetId: str
 
             let! response = request.ExecuteAsync()
             let values = response.Values
+
             let values =
                 if not (isNull values) then
                     values |> Seq.map (Seq.map string)
@@ -34,8 +35,20 @@ type Sheet internal (service: SheetsService, spreadsheetId: string, sheetId: str
 
             let maxRowLength = values |> Seq.map Seq.length |> Seq.max
 
-            return values
+            return
+                values
                 |> Seq.map (fun row -> Seq.append row (Seq.replicate (maxRowLength - Seq.length row) ""))
+        }
+
+    let sheetIndexAsync spreadsheetId =
+        task {
+            let! spreadsheet = service.Spreadsheets.Get(spreadsheetId).ExecuteAsync()
+
+            return
+                (spreadsheet.Sheets
+                 |> Seq.filter (fun s -> s.Properties.Title = sheetId)
+                 |> Seq.exactlyOne)
+                    .Properties.SheetId
         }
 
     /// Writes a column to a sheet.
@@ -43,13 +56,41 @@ type Sheet internal (service: SheetsService, spreadsheetId: string, sheetId: str
         task {
             let offset = defaultArg offset 1
             checkOffset offset
-            let range = sheetId + "!" + column + (string offset) + ":" + column + (string ((data |> Seq.length) + offset))
+            let range = $"{sheetId}!{column}{offset}:{column}{(data |> Seq.length) + offset}"
             let valueRange = ValueRange(Values = [| data |> Seq.cast<obj> |> Seq.toArray |])
             valueRange.MajorDimension <- "COLUMNS"
             let request = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, range)
-            request.ValueInputOption <- Nullable(SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW)
 
-            request.Execute() |> ignore
+            request.ValueInputOption <-
+                Nullable(SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW)
+
+            return! request.ExecuteAsync()
+        }
+
+    /// Writes range to a sheet.
+    member _.WriteSheetAsync(startColumn: string, endColumn: string, data: #seq<seq<string>>, ?offset: int) =
+        task {
+            let offset = defaultArg offset 1
+            checkOffset offset
+
+            let range =
+                $"{sheetId}!{startColumn}{offset}:{endColumn}{(data |> Seq.length) + offset}"
+
+            let dataAsList =
+                data
+                |> Seq.map (Seq.cast<obj>)
+                |> Seq.map ResizeArray
+                |> Seq.cast<System.Collections.Generic.IList<_>>
+                |> ResizeArray
+
+            let valueRange = ValueRange(Values = dataAsList)
+            valueRange.MajorDimension <- "COLUMNS"
+            let request = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, range)
+
+            request.ValueInputOption <-
+                Nullable(SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW)
+
+            return! request.ExecuteAsync()
         }
 
     /// Reads given range of a sheet and returns a sequence of rows in that range.
@@ -63,9 +104,9 @@ type Sheet internal (service: SheetsService, spreadsheetId: string, sheetId: str
             return result |> Seq.concat
         }
 
-    /// Assumes that the offset row is a row with headings and reads only those columns. 
+    /// Assumes that the offset row is a row with headings and reads only those columns.
     /// Returns a list of maps that map header names to row values.
-    member _.ReadByHeadersAsync(columnNames: string list, ?offset: int, ?size: int): Task<seq<Map<string, string>>> =
+    member _.ReadByHeadersAsync(columnNames: string list, ?offset: int, ?size: int) : Task<seq<Map<string, string>>> =
         task {
             let offset = defaultArg offset 1
             let size = defaultArg size 1000
@@ -77,34 +118,102 @@ type Sheet internal (service: SheetsService, spreadsheetId: string, sheetId: str
 
             let! response = request.ExecuteAsync()
             let values = response.Values
+
             if not (isNull values) then
                 let values = values |> Seq.map (Seq.map string)
                 let maxRowLength = values |> Seq.map Seq.length |> Seq.max
-                let values = values |> Seq.map (fun row -> Seq.append row (Seq.replicate (maxRowLength - Seq.length row) ""))
+
+                let values =
+                    values
+                    |> Seq.map (fun row -> Seq.append row (Seq.replicate (maxRowLength - Seq.length row) ""))
+
                 let headersRow = values |> Seq.head
+
                 return
-                    values 
+                    values
                     |> Seq.skip 1
-                    |> Seq.map (
-                        fun row -> 
-                            let zippedRow = Seq.zip headersRow row
-                            zippedRow
-                            |> Seq.filter (fun (h, _) -> columnNames.Contains h)
-                            |> Map.ofSeq
-                       )
+                    |> Seq.map (fun row ->
+                        let zippedRow = Seq.zip headersRow row
+                        zippedRow |> Seq.filter (fun (h, _) -> columnNames.Contains h) |> Map.ofSeq)
             else
                 return Seq.empty
         }
 
+    /// Clears given range.
+    member _.ClearRangeAsync(range: string) =
+        task {
+            let range = $"'{sheetId}'!{range}"
+
+            let request =
+                service.Spreadsheets.Values.Clear(new ClearValuesRequest(), spreadsheetId, range)
+
+            return! request.ExecuteAsync()
+        }
+
+    /// Merges cells denoted by their indexes of start and end row, and start and end column of the merge zone.
+    member _.MergeCellsAsync(cellsToMerge: seq<ValueTuple<int, int, int, int>>) =
+        task {
+            let! sheetIndex = sheetIndexAsync sheetId
+
+            let requests =
+                cellsToMerge
+                |> Seq.map (fun mergeZone ->
+                    let struct (startRowIndex, endRowIndex, startColumnIndex, endColumnIndex) =
+                        mergeZone
+
+                    new MergeCellsRequest(
+                        Range =
+                            new GridRange(
+                                SheetId = sheetIndex,
+                                StartRowIndex = startRowIndex,
+                                EndRowIndex = endRowIndex,
+                                StartColumnIndex = startColumnIndex,
+                                EndColumnIndex = endColumnIndex
+                            ),
+                        MergeType = "MERGE_COLUMNS"
+                    ))
+                |> Seq.cast<Request>
+                |> ResizeArray
+
+            let request =
+                service.Spreadsheets.BatchUpdate(new BatchUpdateSpreadsheetRequest(Requests = requests), spreadsheetId)
+
+            return! request.ExecuteAsync()
+        }
+
+    /// Unmerges all cells in a sheet.
+    member _.UnmergeCellsAsync() =
+        task {
+            let! spreadsheet = service.Spreadsheets.Get(spreadsheetId).ExecuteAsync()
+
+            let sheetIndex =
+                (spreadsheet.Sheets
+                 |> Seq.filter (fun s -> s.Properties.Title = sheetId)
+                 |> Seq.exactlyOne)
+                    .Properties.SheetId
+
+            let unmergeRequest =
+                Request(UnmergeCells = UnmergeCellsRequest(Range = new GridRange(SheetId = sheetIndex)))
+
+            let request =
+                service.Spreadsheets.BatchUpdate(
+                    new BatchUpdateSpreadsheetRequest(Requests = ResizeArray([ unmergeRequest ])),
+                    spreadsheetId
+                )
+
+            return! request.ExecuteAsync()
+        }
+
+
 /// Represents a single Google Sheets document.
 type Spreadsheet internal (service: SheetsService, spreadsheetId: string) =
     /// Returns sheet object by Id. Does not query Google Sheets servers, just provides proxy.
-    member _.Sheet (sheetId: string) = Sheet(service, spreadsheetId, sheetId)
+    member _.Sheet(sheetId: string) = Sheet(service, spreadsheetId, sheetId)
 
     /// Returns a list of sheet ids (or tabs) in a given document. Does query server. Actual sheets can then be retrieved by id.
-    member _.GetSheetsAsync () =
+    member _.GetSheetsAsync() =
         task {
-            let! spreadsheet = service.Spreadsheets.Get(spreadsheetId).ExecuteAsync ()
+            let! spreadsheet = service.Spreadsheets.Get(spreadsheetId).ExecuteAsync()
             return spreadsheet.Sheets |> Seq.map (fun s -> s.Properties.Title)
         }
 
@@ -112,32 +221,35 @@ type Spreadsheet internal (service: SheetsService, spreadsheetId: string) =
 type GoogleSheetService private (service: SheetsService) =
     static member CreateAsync(credentialsFileName: string, applicationName: string) =
         task {
-            use credentialsStream = new FileStream(credentialsFileName, FileMode.Open, FileAccess.Read)
+            use credentialsStream =
+                new FileStream(credentialsFileName, FileMode.Open, FileAccess.Read)
 
-            let! credential = 
+            let! credential =
                 GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.FromStream(credentialsStream).Secrets,
                     [ SheetsService.Scope.Spreadsheets ],
                     "user",
                     CancellationToken.None,
-                    FileDataStore("token.json", true))
+                    FileDataStore("token.json", true)
+                )
 
-            return new GoogleSheetService(new SheetsService(
-                    BaseClientService.Initializer(
-                        HttpClientInitializer = credential,
-                        ApplicationName = applicationName
+            return
+                new GoogleSheetService(
+                    new SheetsService(
+                        BaseClientService.Initializer(
+                            HttpClientInitializer = credential,
+                            ApplicationName = applicationName
+                        )
                     )
                 )
-            )
         }
 
     /// Gets a document by its id (hash). Does not actually query servers, returns proxy.
-    member _.Spreadsheet (spreadsheetId: string) =
-        Spreadsheet(service, spreadsheetId)
+    member _.Spreadsheet(spreadsheetId: string) = Spreadsheet(service, spreadsheetId)
 
     /// Gets sheet (a tab in a document) by document id (hash) and sheet id (tab name). Does not query servers.
-    member _.Sheet (spreadsheetId: string, sheetId: string) = 
+    member _.Sheet(spreadsheetId: string, sheetId: string) =
         Spreadsheet(service, spreadsheetId).Sheet(sheetId)
 
     interface IDisposable with
-        member _.Dispose () = service.Dispose ()
+        member _.Dispose() = service.Dispose()
